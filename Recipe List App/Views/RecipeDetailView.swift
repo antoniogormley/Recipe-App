d2b286774a9e2025bef4e75d543622542d1cb26f
Antonio Gormley <antoniogormley@gmail.com>
2021-06-27 18:15:17 +0100

    -button for recipe cards -display recipe detail ^ -move recipe title

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe:Recipe
+    @State var selectedServingSize = 2
     
     var body: some View {
         ScrollView {
@@ -20,13 +21,35 @@ struct RecipeDetailView: View {
                 Image(recipe.image)
                     .resizable()
                     .scaledToFill()
+                
+                Text(recipe.name)
+                    .bold()
+                    .padding(.top,20)
+                    .padding(.leading)
+                    .font(.largeTitle)
+                
+                VStack (alignment: .leading ){
+                    Text("Select your serving size: ")
+                    Picker("",selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
+                    
+                }
+                .padding()
+                
                 VStack(alignment: .leading) {
                 Text("Ingredients")
                     .font(.headline)
                     .padding([.bottom,.top], 5)
 
                     ForEach (recipe.ingredients) { item in
-                        Text("• " + item.name)
+                        Text("• " + RecipeModel.getPortion(ingredient: item,recipeServings: recipe.servings ,targetServings: selectedServingSize) + " " + item.name.lowercased())
                     }
                 }
                 .padding(.horizontal)
@@ -47,7 +70,7 @@ struct RecipeDetailView: View {
 
             }
         }
-        .navigationBarTitle(recipe.name)
+
 
     }
 }
